981066594970c273b23d3b45dfecdac25a9479e7
homerOurs <homerours@gmail.com>
2020-06-17 10:27:26 +0200

    installer

--- installer.sh
+++ installer.sh
@@ -12,6 +12,7 @@ function install_this() {
 
 	if [[ $1 == "symlink" ]]
 	then
+		echo "SYMLINK"
 		olddir=~/dotfiles_old             # old dotfiles backup directory
 		files="vimrc zshrc tmux.conf gitconfig bashrc inputrc dircolors.ansi-dark"    # list of files/folders to symlink in homedir
 		mkdir -p $olddir
@@ -25,12 +26,19 @@ function install_this() {
 	then
 		echo "Installing Prezto!"
 		$dir/install_prezto.sh
-	elif [[ $1 == "plug-nvim" ]]
+	elif [[ $1 == "nvim-plug" ]]
 	then
+		echo "Installing Plug for NEOVIM!"
 		curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	elif [[ $1 == "plug-vim" ]]
+	elif [[ $1 == "vim-plug" ]]
 	then
+		echo "Installing Plug for VIM!"
 		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	elif [[ $1 == "fzf" ]]
+	then
+		echo "Installing FZF!"
+		git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+		~/.fzf/install
 	else
 		echo "Option $1 unknown"
 	fi
